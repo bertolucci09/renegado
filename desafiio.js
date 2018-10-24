990c46b85943211218909941498f4ee56bddc1c2
bertolucci09 <44439368+bertolucci09@users.noreply.github.com>
2018-10-24 11:31:00 -0300

    Add files via upload

---
+++ desafiio.js
@@ -0,0 +1,18 @@
+var nome=prompt("quanto tepo eu tenho para tomar este suco?");
+var contadorDaCorrida = 5;
+while (contadorDaCorrida > 0){
+    alert("em "+contadorDaCorrida+"s!");
+    contadorDaCorrida --;
+}
+alert("voce perdeu o desafio");
+var w;
+function iniciar() {
+    if(typeof(button) !== "undefined");
+        if(typeof(w) == "undefined");
+           
+
+
+function parar() { 
+    w.terminate();
+    w = undefined;
+}
---
+++ quarta.js
@@ -0,0 +1,3 @@
+var nome=prompt("qual o seu nome?");
+alert("este site foi elaborado por jonathann "+ nome);
+
